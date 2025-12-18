7865ad1d90a0a758a1e8ac913c030dc97b25c98d
ENavarro <angelasthefany00@gmail.com>
2025-12-18 16:13:17 -0300

    login e registro estilizações

--- config_example.py
+++ config_example.py
@@ -1,7 +1,9 @@
+# import os
 # class Config:
 #   DEBUG = True
 
 #   SQLALCHEMY_DATABASE_URI = "postgresql://user:password@localhost:5432/database"
 #   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+#   UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
 #   SECRET_KEY = 'sua_secret_key'
